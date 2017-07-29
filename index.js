7245dedebc3ebd700b535aad663ccc475b44ef46
James Thieu <jamesthieu23@guest-wireless-207-151-059-066.usc.edu>
2017-07-28 19:08:35 -0700

    sad reacts only

--- index.js
+++ index.js
@@ -33,68 +33,11 @@ app.use(function(req, res, next) {
    next();
 });
 
-//var Mentor = mongoose.model('Mentor',{
-//	first_name: String,
-//	last_name: String,
-//	username: String,
-//	password: String,
-//	phone_number: String,
-//	major: String,
-//	job_position: String,
-//	education: String,
-//	dream_career: String,
-//	bio: String,
-//	age: Number
-//});
-
-//app.get('/api/mentors', function(req,res){
-//	console.log("fetching mentors");
-//	Mentor.find(function(err, mentors){
-//		if (err)
-//			res.send(err);
-//		res.json(mentors);
-//	});
-//});
-
-//app.get('/', function(req,res) {
-//    console.log("We're in.");
-//});
-
-//app.post('/api/mentors', function(req, res){
-//	console.log("fetching mentors");
-//	Mentor.create({
-//		first_name: req.body.first_name,
-//		last_name: req.body.last_name,
-//		phone_number: req.body.phone_number,
-//		username: req.body.username,
-//		password: req.body.password,
-//		major: req.body.major,
-//		job_position: req.body.job_position,
-//		education: req.body.education,
-//		dream_career: req.body.dream_career,
-//		bio: req.body.bio,
-//		age: req.body.age,
-//		done: false
-//	
-//	}, function(err, mentor){
-//		if (err)
-//			res.send(err);
-//		Mentor.find(function(err, mentors){
-//		if (err)
-//			res.send(err);
-//		res.json(mentors);
-//	});
-//
-//	});
-//});
-
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
     console.log("We're in.");
     }, function (err, doc) {
 
     });
-
-})
 
 app.get("/pullEvents", function (req, res) {
     events.find().toArray(function (err, docs) {
@@ -107,8 +50,8 @@ app.get("/pullEvents", function (req, res) {
             })
             res.json(result);
         }
-    })
-})
+    });
+});
 
 app.get("/pullEvent", function (req, res) {
     events.find({
@@ -121,8 +64,8 @@ app.get("/pullEvent", function (req, res) {
 
             res.json(docs);
         }
-    })
-})
+    });
+});
 app.post("/pushOrder", (req, res) => {
     /*orders.insert(req, (err, doc) => {
         if (err) {
@@ -135,7 +78,7 @@ app.post("/pushOrder", (req, res) => {
     }) */
     console.log(req.body);
 
-})
+});
 
 
 var port = process.env.PORT || 8080;
